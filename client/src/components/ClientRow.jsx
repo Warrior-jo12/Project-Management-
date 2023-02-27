afaec66b3127c47cba29bfe255a093737e3844fd
warrior-jo12 <eyutialemu@gmail.com>
2023-02-27 21:44:57 +0300

    components to create grraphql client for the client schema created

---
+++ client/src/components/ClientRow.jsx
@@ -0,0 +1,36 @@
+import {FaTrash} from 'react-icons/fa'
+import { DELETE_CLIENT } from '../mutations/clientMutation'
+import { useMutation } from '@apollo/client'
+import { GET_CLIENTS } from '../Queries/ClientQueries'
+import {GET_PROJECTS} from '../Queries/projectQueries'
+
+const ClientRow = ({client}) => {
+    const [deleteClient] = useMutation(DELETE_CLIENT, {
+        variables:{id:client.id},
+        refetchQueries: [{query: GET_CLIENTS}, {query:GET_PROJECTS}]
+        // update(cache, {data: {deleteClient}}){
+        //     const {clients} = cache.readQuery({
+        //         query: GET_CLIENTS});
+        //         cache.writeQuery({
+        //             query: GET_CLIENTS,
+        //             data: {
+        //                clients: clients.filter(client => client.id !== deleteClient.id)
+        //             },
+        //         });
+        // },
+    });
+    return(
+        <tr>
+            <td>{client.name}</td>
+            <td>{client.email}</td>
+            <td>{client.phone}</td>
+            <td>
+                <button onClick={deleteClient} className='btn btn-danger btn-sm'>
+                    <FaTrash/>
+                </button>
+            </td>
+        </tr>
+    )
+}
+
+export default ClientRow
